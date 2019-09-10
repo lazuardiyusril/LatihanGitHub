b5a8cd6a00005a1e199f130db6df7ab192936a15
lazuardiyusril <54520006+lazuardiyusril@users.noreply.github.com>
2019-09-10 11:50:43 +0700

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,2 +1,12 @@
-print("ini test")
-print("line tambahan")
+a = []
+d = []
+f = []
+for i in range(1,10):
+    a.append(i)
+for item in range(0,3):   
+    d.append(list(a[(3 * item):(3 + 3 * item)]))
+for item in range(0, 3):
+    e = []
+    for item in range(len(a) - (2-item),0,-3):
+        e.append(item)
+    print(e)
